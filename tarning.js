1ecd9ce58ab8086cb0987eda033b9fe2ab243fbf
rebeks314 <rebecka.ekstrom@hotmail.se>
2015-03-06 13:18:18 +0100

    funktioner

--- tarning.js
+++ tarning.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 //    console.log("hej");
-    $(".kasta").click(function() {
+    $(".knapp").click(function() {
         var x = Math.floor((Math.random() * 6) + 1);
        var n = x.toString();
-        $('<p>').text(n).appendTo("#box1");
+        $('<p>').text(n).appendTo("#1");
         console.log(n);
    
     });
