1593dc4c2bee50549afb5d5155be184b1a090403
rebeks314 <rebeks314@E29660.edu.linkoping.se>
2015-03-06 14:55:45 +0100

    tärningar

--- tarning.js
+++ tarning.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-//    console.log("hej");
-    $(".knapp").click(function() {
-        var x = Math.floor((Math.random() * 6) + 1);
-       var n = x.toString();
-        $('<p>').text(n).appendTo("#1");
-        console.log(n);
+
+addListener();
    
-    });
+//en "lyssnare" till knappen
+    function addListener() {
+            $(".knapp").click(function() {
+            var dice = rollDice();
+            $('<p>').text(dice).appendTo(".ruta");
+
+        });
+    }
+//slumpgenerator
+    function rollDice() {
+        var x = Math.floor((Math.random() * 6) + 1);
+        var n = x.toString();
+        return n;
+    }
+
 });
 
 
