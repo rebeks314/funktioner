211dcf5f5e8164b41a73f9608a7808653cc1a826
rebeks314 <rebecka.ekstrom@hotmail.se>
2015-03-10 13:49:16 +0100

    loop

--- tarning.js
+++ tarning.js
@@ -1,22 +1,41 @@
 $(document).ready(function() {
 
-addListener();
-   
-//en "lyssnare" till knappen
-    function addListener() {
-            $(".knapp").click(function() {
-            var dice = rollDice();
-            $('<p>').text(dice).appendTo(".ruta");
+    addListener();
+    
+    
 
-        });
-    }
-//slumpgenerator
-    function rollDice() {
-        var x = Math.floor((Math.random() * 6) + 1);
-        var n = x.toString();
-        return n;
-    }
 
 });
 
+
+//en "lyssnare" till knappen
+function addListener() {
+    $(".knapp").click(function() {
+        var numberDice = getDice();
+        var dice = rollDice();
+        console.log("numberDice: "+numberDice);
+        $('<p>').text(dice).appendTo(".ruta");
+
+    });
+}
+//slumpgenerator
+function rollDice() {
+    var x = Math.floor((Math.random() * 6) + 1);
+    var n = x.toString();
+    return n;
+    
+}
+
+function getDice(){
+    console.log(" getdice körs");
+    return $('.radioKnapp:checked').val();
+    
+    for( var i = 0; i < numberDice; i++){
+        var x = Math.floor((Math.random() * 6) + 1);
+        summa += x;
+        
+        
+    }
+    
+}
 
